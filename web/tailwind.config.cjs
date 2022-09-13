b04a1a0232dcd4f08bfc9cfb9813dcf43b084709
Xandowski <xandy-moraes@hotmail.com>
2022-09-13 01:54:11 -0300

    feat: landing page

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,9 +2,19 @@
 module.exports = {
   content: [
     './src/**/*.tsx',
+    './index.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background.png')",
+        'duo-gradient': 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 60.94%, #E1D55D 16.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
+      }
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    }
   },
   plugins: [],
 }
