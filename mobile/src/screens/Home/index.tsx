9f94662e948c10425385d1fa71051a6e91ed2f07
Xandowski <xandy-moraes@hotmail.com>
2022-09-13 22:36:42 -0300

    initial config

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,15 @@
+import { View, Image } from 'react-native'
+import { styles } from './styles'
+
+import { logo } from '../../assets/logo-nlw-esports.png'
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image 
+        source={logo}
+        style={styles.logo}
+      />
+    </View>
+  )
+}
